a9f52c16f27b51878a9c79df874f91c6ae7911db
yabgu15 <altinkaynakyabgu@gmail.com>
2026-05-03 14:11:31 +0300

    int float str bool

---
+++ mathhh.py
@@ -0,0 +1,20 @@
+import math
+
+x = 3.1
+
+def round_up(x):
+    return print(math.ceil(x))
+
+def round_down(x):
+    return print(math.floor(x))
+
+round_down(x)
+round_up(x)
+
+
+
+a=3 #int değer tanımladım
+b=True #bool değer tanımladım
+c=False #bool değer tanımladım
+d="sabrina" #string değer tanımladım
+f=6.9 #float değer tanımladım
